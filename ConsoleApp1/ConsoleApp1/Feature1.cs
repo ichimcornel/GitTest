3df9c1b7f4d742af983eb0f8d125be7b1a9ec5e4
Cornel Ichim <ichimcornel@ymail.com>
2018-01-02 19:46:38 +0200

    Added an Add method to Feature 1

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -6,5 +6,13 @@ namespace ConsoleApp1
 {
     class Feature1
     {
+
+        public int Add()
+        {
+            int x = 34;
+            int y = 45;
+            int sum = x + y;
+            return sum;
+        }
     }
 }
